b345b539b4c7c42b261d354671a7a89d220d32cb
karl <karl>
2006-11-30 07:45:20 +0000

    A crude comparison against the pre boyer-moore stuff that makes the old way look a bit worse than it really was.

--- ctables/bigtests/match-test.tcl
+++ ctables/bigtests/match-test.tcl
@@ -10,5 +10,15 @@ proc test1 {} {
     puts ""
 }
 
-time test1
+puts [time test1]
+puts [time test1]
 
+proc test2 {} {
+    puts "matching *bernadine*piersol*"
+
+    n search -compare {{match name "*bernadine*piersol*"}} -write_tabsep stdout
+    puts ""
+}
+
+puts [time test2]
+puts [time test2]
