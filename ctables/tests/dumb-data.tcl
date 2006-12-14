d775b02e1107c2c4f9830c759a3173dd832bdcce
karl <karl>
2006-12-14 06:53:23 +0000

    Add id that matches the key so "sort+" can get the kind of responses we expect "sort" to get.  I am leaning towards the key always existing in the table as a named field.

--- ctables/tests/dumb-data.tcl
+++ ctables/tests/dumb-data.tcl
@@ -11,6 +11,7 @@ package require ctable
 CExtension searchtest 1.0 {
 
 CTable testTable {
+    varstring name indexed 1 unique 1
     varstring name indexed 1
     varstring home indexed 1 unique 0
     varstring show indexed 1 unique 0
@@ -30,47 +31,47 @@ testTable create t
 set show "Venture Bros"
 set home "Venture Compound"
 
-t set brock name "Brock Sampson" show $show home $home age 35 coolness 100
-t set hank name "Hank Venture" show $show home $home dad rusty age 16 coolness -6
-t set dean name "Dean Venture" show $show home $home dad rusty age 16 coolness -10
-t set jonas name "Doctor Jonas Venture" show $show home $home alive 0 age 60 coolness 60
-t set orpheus name "Doctor Orpheus" show $show home $home age 55 coolness 30
-t set triana name "Triana" show $show home $home gender female dad orpheus age 16 coolness 50
-t set rusty name "Rusty Venture" show $show home $home dad jonas age 45 coolness 6
-t set jonas_jr name "Doctor Jonas Venture Junior" show $show home "Spider Skull Island" dad jonas age 45 coolness 120
+t set brock id brock name "Brock Sampson" show $show home $home age 35 coolness 100
+t set hank id hank name "Hank Venture" show $show home $home dad rusty age 16 coolness -6
+t set dean id dean name "Dean Venture" show $show home $home dad rusty age 16 coolness -10
+t set jonas id jonas name "Doctor Jonas Venture" show $show home $home alive 0 age 60 coolness 60
+t set orpheus id orpheus name "Doctor Orpheus" show $show home $home age 55 coolness 30
+t set triana id triana name "Triana" show $show home $home gender female dad orpheus age 16 coolness 50
+t set rusty id rusty name "Rusty Venture" show $show home $home dad jonas age 45 coolness 6
+t set jonas_jr id jonas_jr name "Doctor Jonas Venture Junior" show $show home "Spider Skull Island" dad jonas age 45 coolness 120
 
 set home "The Cocoon"
-t set doctor_girlfriend name "Doctor Girlfriend" show $show home $home gender female age 30 coolness 80
-t set the_monarch name "The Monarch" show $show home $home age 40 coolness 36
-t set 21 name "Number 21" show $show home $home age 35 coolness 7
-t set 28 name "Number 28" show $show home $home age 36 coolness 9
+t set doctor_girlfriend id doctor_girlfriend name "Doctor Girlfriend" show $show home $home gender female age 30 coolness 80
+t set the_monarch id the_monarch name "The Monarch" show $show home $home age 40 coolness 36
+t set 21 id 21 name "Number 21" show $show home $home age 35 coolness 7
+t set 28 id 28 name "Number 28" show $show home $home age 36 coolness 9
 
-t set phantom_limb name "Phantom Limb" show $show age 44 coolness 40
-t set baron name "Baron Unterbheit" show $show age 38 coolness -100
+t set phantom_limb id phantom_limb name "Phantom Limb" show $show age 44 coolness 40
+t set baron id baron name "Baron Unterbheit" show $show age 38 coolness -100
 
 set show "ATHF"
 set home "Next To Carl"
 
-t set meatwad name "Meatwad" show $show home $home  age 4 coolness -7
-t set shake name "Master Shake" show $show home $home  age 4 coolness -5
-t set frylock name "Frylock" show $show home $home age 4 coolness 8
-t set carl name "Carl" show $show home "New Jersey" age 43 coolness 5
-t set inignot name "Inignot" show $show home "The Moon" age 2 coolness 31
-t set ur name "Ur" show $show home "The Moon" age 1 coolness 26
+t set meatwad id meatwad name "Meatwad" show $show home $home  age 4 coolness -7
+t set shake id shake name "Master Shake" show $show home $home  age 4 coolness -5
+t set frylock id frylock name "Frylock" show $show home $home age 4 coolness 8
+t set carl id carl name "Carl" show $show home "New Jersey" age 43 coolness 5
+t set inignot id inignot name "Inignot" show $show home "The Moon" age 2 coolness 31
+t set ur id ur name "Ur" show $show home "The Moon" age 1 coolness 26
 
 set show "The Brak Show"
-t set dad name "Dad" show $show age 44 coolness 37
-t set brak name "Brak" show $show dad dad age 15 coolness 4
-t set zorak name "Zorak" show $show age 16 coolness 64
-t set mom name "Mom" show $show gender female age 41 coolness 101
-t set thundercleese name "Thundercleese" show $show age 6 coolness 16
-t set clarence name "Clarence" show $show age 15 coolness -11
+t set dad id dad name "Dad" show $show age 44 coolness 37
+t set brak id brak name "Brak" show $show dad dad age 15 coolness 4
+t set zorak id zorak name "Zorak" show $show age 16 coolness 64
+t set mom id mom name "Mom" show $show gender female age 41 coolness 101
+t set thundercleese id thundercleese name "Thundercleese" show $show age 6 coolness 16
+t set clarence id clarence name "Clarence" show $show age 15 coolness -11
 
 set show "Stroker and Hoop"
-t set stroker name "John Strokmeyer" show $show age 40 coolness 33
-t set hoop name "Hoop" show $show age 38 coolness 14
-t set angel name "Angel" show $show gender female age 38 coolness 17
-t set carr name "C. A. R. R." show $show age 2 coolness 29
-t set rick name "Coroner Rock" show $show age 51 coolness 99
+t set stroker id stroker name "John Strokmeyer" show $show age 40 coolness 33
+t set hoop id hoop name "Hoop" show $show age 38 coolness 14
+t set angel id angel name "Angel" show $show gender female age 38 coolness 17
+t set carr id carr name "C. A. R. R." show $show age 2 coolness 29
+t set rick id rick name "Coroner Rock" show $show age 51 coolness 99
 
 
