d3b0b5a2d6185d1cc0ddd8db30628f0c505fea97
karl <karl>
2006-12-20 10:17:51 +0000

    Add a couple of search+ tests.

--- ctables/tests/client-test.tcl
+++ ctables/tests/client-test.tcl
@@ -19,3 +19,14 @@ puts "search of t in descending coolness limit 5"
 t search -sort -coolness -limit 5 -write_tabsep stdout
 puts ""
 
+puts "search+ of t in descending coolness limit 5 / code body"
+puts [t index indexable]
+t index create show
+
+t search+ -sort -coolness -limit 5 -key key -array_get_with_nulls data -code {puts "$key -> $data"}
+puts ""
+
+puts "search+ of t in descending coolness limit 5"
+t search+ -sort -coolness -limit 5 -write_tabsep stdout
+puts ""
+
