0688fd359bd052b8ca5124f6dd9d4e0819555092
peter <peter>
2007-01-18 17:36:21 +0000

    Clean up client.tcl, add package:///table[/path].

--- stapi/client/client.tcl
+++ stapi/client/client.tcl
@@ -27,9 +27,9 @@ namespace eval ::scache {
 
   # ctable://[host:port]/[dir/]table[/stuff][?stuff]
   variable ctable_serial 0
-  proc connect_ctable {table {address "localhost"} args} {
+  proc connect_ctable {table_path {address "localhost"} args} {
     variable ctable_serial
-    set uri ctable://$address/$table
+    set uri ctable://$address/$table_path
     set local_table ::scache::ctable[incr ctable_serial]
     remote_ctable $uri $local_table
     return $local_table
@@ -37,13 +37,31 @@ namespace eval ::scache {
   register ctable connect_ctable
 
   # local:///ctable_name - dummy connector for local tables
-  proc connect_local {table {address "-"} args} {
+  proc connect_local {table {address ""} args} {
     if {"$address" != "*" && "$address" != ""} {
       return -code error "Local connection can not be made to remote host"
     }
     return [uplevel 2 [list namespace which $table]]
   }
   register local connect_local
+
+  # package:///package_name/table[/path]
+  proc connect_package {path {address ""} args} {
+    if [regexp {^/*([^/]+)(/.*)} $path _ table path] {
+      if [file isdirectory $path/$table] {
+        set path $path/$table
+      }
+      if {[lsearch -exact $::auto_path $path] == -1} {
+        lappend auto_path $path
+      }
+    } else {
+      set table $path
+    }
+    set package [string totitle $table 0 0]
+    uplevel 0 [list package require $package]
+    return [$table #auto]
+  }
+  register package connect_package
 }
 
 package provide scache_client 1.0
