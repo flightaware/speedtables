dc2805208ba0e395f899954b828512bda5709665
peter <peter>
2007-07-24 23:43:58 +0000

    need for setting up shared ctable

---
+++ ctables/tests/master_server.tcl
@@ -0,0 +1,34 @@
+#
+# make sure the auto row ID thing is working when read_tabsepping with nokeys
+#
+# $Id$
+#
+
+package require ctable_server
+
+set suffix _m
+
+source top-brands-nokey-def.tcl
+
+
+top_brands_nokey_m create m master sharefile.dat
+
+proc suck_in_top_brands_nokeys {} {
+    set fp [open top-brands.tsv]
+    set lastKey [m read_tabsep $fp -nokeys]
+    close $fp
+    if {"$lastKey" == ""} {
+	error "should have returned next key value"
+    }
+    return $lastKey
+}
+
+suck_in_top_brands_nokeys
+
+
+::ctable_server::register ctable://*/master m
+
+puts "running, waiting for connections"
+
+if !$tcl_interactive { vwait die }
+
