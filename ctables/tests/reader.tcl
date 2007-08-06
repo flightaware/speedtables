7a426aa88db14f9f1d37438f749a8758b7a7d911
peter <peter>
2007-08-06 00:09:38 +0000

    snapshot tests

--- ctables/tests/reader.tcl
+++ ctables/tests/reader.tcl
@@ -19,5 +19,23 @@ top_brands_nokey_m create r reader $params
 
 puts "created reader r"
 
-r search -key k -array_get a -code {puts "$k : $a"}
+r search -key k -array_get a -code {set orig($k) $a}
+set names [array names orig]
+set num [llength $names]
+
+puts "looping"
+
+for {set i 0} {$i < 1000} {incr i} {
+    set n [expr {int(rand() * $num)}]
+    set k [lindex $names $n]
+    array set row $orig($k)
+    r search -compare [list [list = id $row(id)]] -key k -array_get a -code {
+	puts [format "%4d %4d %s : %s" $i $n $k $a]
+    }
+    after 15
+}
+
+puts "shutting down"
+
+r shutdown
 
