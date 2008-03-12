5157d35e60bdafc32fe0cc920717417b49c44c13
quengho <quengho>
2008-03-12 00:28:36 +0000

    sync

--- ctables/demos/_setup.sh
+++ ctables/demos/_setup.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
 # $Id$
 
-for format in zip html pdf
-do
-  ln -f -s ../docs/speedtables.$format .
-done
-
 for file in _setup.sh getuser_client getuser_direct getuser_shared getuser_sql getuser_sttp passwd_server.tcl populate_sql.tcl shared_server.tcl
 do
   chmod +x $file
