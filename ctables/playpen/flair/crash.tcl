3c5b1f3037b03e58aeb2af9c18ef375ebc270dd2
karll <karll>
2009-03-11 06:00:12 +0000

    Crash ctables.

---
+++ ctables/playpen/flair/crash.tcl
@@ -0,0 +1,28 @@
+#
+# crash ctables circa 3/2009 with table operations
+#
+# $Id$
+#
+
+package require ctable
+
+CExtension Meowmix 1.1 {
+
+CTable Meow {
+    varstring value indexed 1 default stinky
+}
+
+}
+
+package require Meowmix
+
+Meow create m
+m index create value
+
+#Meow null_value stinkpot
+#Meow null_value ""
+
+m set 0 value {}
+m set 4 value 3104
+m set 0 value 4728
+m delete 4
