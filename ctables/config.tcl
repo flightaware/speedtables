b2f20c7aa40141513d8a16d6f1ac2b5ea471c046
Karl Lehenbauer <karl@karl.gwp.corp.flightaware.com>
2009-06-18 11:20:34 -0500

    Turn off inlining in config.tcl.  We came across a case where gcc 4.2.1 generated incorrect infinite-loop-causing code when compiling the inlined skiplist routines.

--- ctables/config.tcl
+++ ctables/config.tcl
@@ -7,11 +7,13 @@
     variable withPipe 1
 
     # set to 1 to build with debugging and link to tcl debugging libraries
-    set genCompilerDebug 1
+    set genCompilerDebug 0
+
     # set to 1 to link to mem debug libraries
     set memDebug 0
+
     # Set to 0 to import some code without inline.
-    set fullInline 1
+    set fullInline 0
 
     # set to 1 to show compiler commands
     set showCompilerCommands 0
@@ -52,4 +54,3 @@
     }
 
 
-set withPgtcl 1
