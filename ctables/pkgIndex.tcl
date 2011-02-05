6fffb8d8135f2d199b94c31b946ba457145478f6
Jeff Lawson <jeff@bovine.net>
2011-02-05 01:51:02 -0600

    remove files now generated by autoconf

--- ctables/pkgIndex.tcl
+++
@@ -1,9 +0,0 @@
-# $Id$
-namespace eval ::ctable set srcDir $dir
-
-# We'll start out making gentable_stub just package require ctable, then
-# switch when all else is kosher. Since the ::ctable namespace isn't
-# accessed directly we don't need to worry about namespaces
-package ifneeded ctable 1.7 [list source [file join $dir gentable.tcl]]
-package ifneeded speedtable 1.7 [list source [file join $dir gentable.tcl]]
-
