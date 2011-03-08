22582f8de8e4e20417006fd19f933397a1eaa6eb
Jeff Lawson <jeff@bovine.net>
2011-03-07 23:20:47 -0600

    add configure/makefile to top-level, stapi, and ctable_server directories (Issue 15)

--- update_ver.sh
+++ update_ver.sh
@@ -6,8 +6,6 @@ NEWVER="1.8.1"
 
 perl -p -i -e "s/^(AC_INIT\\(\\[ctable\\],) \\[[0-9.]+\\]/\\1 \\[$NEWVER\\]/" ctables/configure.in
 
-perl -p -i -e "s/(set ctablePackageVersion) ([0-9.]+)/\\1 $NEWVER/" ctables/gentable.tcl
-
 perl -p -i -e "s/^(package provide st\S+) [0-9.]+/\\1 $NEWVER/" stapi/client/*.tcl stapi/display/*.tcl stapi/server/*.tcl stapi/*.tcl
 
 perl -p -i -e "s/^(package provide \S+) [0-9.]+/\\1 $NEWVER/" ctable_server/*.tcl
