a5c12fa681fd1c9c4100e7a4c9be3abb3b47ad24
Jeff Lawson <jeff@bovine.net>
2011-11-09 15:28:30 -0600

    use Apache Rivet's escape_string/unescape_string (when available) for a more robust implementation.

--- stapi/client/pgsql.tcl
+++ stapi/client/pgsql.tcl
@@ -80,23 +80,35 @@ namespace eval ::stapi {
   # uri_esc - escape a string i think for passing in a URI/URL
   #
   proc uri_esc {string {extra ""}} {
-    foreach c [split "%\"'<> $extra" ""] {
-      scan $c "%c" i
-      regsub -all "\[$c]" $string [format "%%%02X" $i] string
-    }
-    return $string
+	  if {[catch {escape_string $string} result] == 0} {
+		  # we were running under Apache Rivet and could use its existing command.
+		  return $result
+	  } else {
+		  # TODO: this is not very good and is probably missing some cases.
+		  foreach c [split "%\"'<> $extra" ""] {
+			  scan $c "%c" i
+			  regsub -all "\[$c]" $string [format "%%%02X" $i] string
+		  }
+		  return $string
+	  }
   }
 
   #
   # uri_unesc - unescape a string after passing it through a URI/URL
   #
   proc uri_unesc {string} {
-    foreach c [split {\\$[} ""] {
-      scan $c "%c" i
-      regsub -all "\\$c" $string [format "%%%02X" $i] string
-    }
-    regsub -all {%([0-9A-Fa-f][0-9A-Fa-f])} $string {[format %c 0x\1]} string
-    return [subst $string]
+	  if {[catch {unescape_string $string} result] == 0} {
+		  # we were running under Apache Rivet and could use its existing command.
+		  return $result
+	  } else {
+		  # TODO: this is not very good and is probably missing some cases.
+		  foreach c [split {\\$[} ""] {
+			  scan $c "%c" i
+			  regsub -all "\\$c" $string [format "%%%02X" $i] string
+		  }
+		  regsub -all {%([0-9A-Fa-f][0-9A-Fa-f])} $string {[format %c 0x\1]} string
+		  return [subst $string]
+	  }
   }
 
   variable sqltable_seq 0
