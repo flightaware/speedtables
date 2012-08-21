ace54b0c12342b0f3a85a08e3c8fbce8fd885b5b
Peter da Silva <resuna@gmail.com>
2012-08-21 17:04:43 -0500

    Turn off compile debug incomitted version.
    BUGZID:

--- ctables/config.tcl
+++ ctables/config.tcl
@@ -7,7 +7,7 @@
     variable withPipe 1
 
     # set to 1 to build with debugging and link to tcl debugging libraries
-    set genCompilerDebug 1
+    set genCompilerDebug 0
 
     # set to 1 to link to mem debug libraries
     set memDebug 0
@@ -19,7 +19,7 @@
     set fullStatic 1
 
     # set to 1 to show compiler commands
-    set showCompilerCommands 1
+    set showCompilerCommands 0
 
 # Less common parameters
 
