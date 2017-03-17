743d0b1f01327a85ecd6321d7f5e6b49bc7217c9
Peter da Silva <peter.dasilva@flightaware.com>
2017-03-17 17:10:02 +0000

    Dump "readers".

--- ctables/shared/dumper.c
+++ ctables/shared/dumper.c
@@ -22,40 +22,59 @@ void usage (char *av0)
 
 int main(int ac, char **av)
 {
-    shm_t *share;
-    char *filename;
-    char *av0 = *av;
+	shm_t          *share;
+	char           *filename;
+	char           *av0 = *av;
 
-    while (*++av) {
-	if(filename == NULL) {
-	    filename = *av;
-	} else {
-	    usage(av0);
-	    exit(-1);
+	while (*++av) {
+		if (filename == NULL) {
+			filename = *av;
+		} else {
+			usage(av0);
+			exit(-1);
+		}
 	}
-    }
 
-    if(filename == NULL) {
-	usage(av0);
-	exit(-1);
-    }
+	if (filename == NULL) {
+		usage(av0);
+		exit(-1);
+	}
+	share = map_file(filename, MAPADDR, 0, 0, 0);
 
-    share = map_file(filename, MAPADDR, 0, 0, 0);
+	if (!share) {
+		fprintf(stderr, "map_file('%s', %08x, 0, 0, 0) failed\n", filename, MAPADDR);
+		exit(2);
+	}
+	printf("FILE %s\n", share->filename);
+	printf("SHARE %s\n", share->name);
+	printf("MAP magic = %08lx headersize = %d mapsize = %d cycle = %d\n",
+	       share->map->magic,
+	       share->map->headersize,
+	       share->map->mapsize,
+	       share->map->cycle);
 
-    if (!share) {
-	fprintf(stderr, "map_file('%s', %08x, 0, 0, 0) failed\n", filename, MAPADDR);
-	exit(2);
-    }
+	int i;
+	int live;
 
-    printf("FILE %s\n", share->filename);
-    printf("SHARE %s\n", share->name);
-    printf("MAP magic = %08lx headersize = %d mapsize = %d cycle = %d\n",
-	share->map->magic,
-	share->map->headersize,
-	share->map->mapsize,
-	share->map->cycle);
+	for(i = 0; i < MAX_SHMEM_READERS; i++) {
+		if(share->map->readers[i].pid) {
+			if(live == 0)
+				printf("READERS:");
+			if(live % 4 == 0)
+				printf("\n    ");
+			else
+				putchar(' ');
 
-    return 0;
+				
+			printf("%5d: %5d %8x;", i,
+				share->map->readers[i].pid, 
+				share->map->readers[i].cycle);
+
+			live++;
+		}
+	}
+
+	printf("NREADERS %d\n", live);
+
+	return	0;
 }
-
-// vim: set ts=8 sw=4 sts=4 noet :
